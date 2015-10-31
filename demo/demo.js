228d5f9ef8bd57fe4ff399a3316fc056d6ea1957
tree.xie <vicansocanbico@gmail.com>
2015-10-31 21:17:34 +0800

    commit

--- demo/demo.js
+++ demo/demo.js
@@ -7,6 +7,7 @@ timtamMongo.init('mongodb://black:27017/timtam');
 
 // get today log
 
+// 只查某天的日志
 timtamMongo.getByDate('test', moment().format('YYYY-MM-DD'), {
 	limit: 3
 }).then(function(res) {
@@ -15,6 +16,39 @@ timtamMongo.getByDate('test', moment().format('YYYY-MM-DD'), {
 	function(err) {
 		console.error(err);
 	});
+
+// 正则匹配出错日志
+timtamMongo.get('test', {
+	message : /RequireJS/gi,
+	level: 'error'
+}).then(function (res) {
+	console.dir(res);
+}, function (err) {
+	// body...
+});
+
+// 统计某天的出错日志数量
+timtamMongo.count('test', {
+	level: 'error',
+	timestamp: {
+		'$gte': moment().format('YYYY-MM-DD') + 'T00:00:00.000Z',
+		'$lte': moment().format('YYYY-MM-DD') + 'T24:00:00.000Z'
+	}
+}).then(function (res) {
+	console.dir(res);
+}, function (err) {
+	// body...
+});
+
+
+// 获取最新的100条日志
+timtamMongo.get('test', {}, {
+	limit: 50
+}).then(function (res) {
+	console.dir(res.length);
+}, function (err) {
+	// body...
+})
 
 // setInterval(function function_name(argument) {
 // 	timtamMongo.add('test', {
